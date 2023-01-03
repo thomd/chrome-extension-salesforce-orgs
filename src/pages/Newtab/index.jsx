4d812ebe5494c2e81971db79eaf592c23e70ccb7
Thomas Dürr <thomduerr@gmail.com>
2023-01-03 17:35:03 +0100

    removed newtab

--- src/pages/Newtab/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import Newtab from './Newtab';
-import './index.css';
-
-createRoot(window.document.querySelector('#app-container')).render(<Newtab />);
-
-if (module.hot) module.hot.accept();
