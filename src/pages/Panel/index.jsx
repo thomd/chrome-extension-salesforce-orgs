df37ad7a39d8687fffea7820eb79270c37b4dedd
Thomas Dürr <thomduerr@gmail.com>
2023-01-03 17:43:35 +0100

    remove devtools

--- src/pages/Panel/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import Panel from './Panel';
-import './index.css';
-
-createRoot(window.document.querySelector('#app-container')).render(<Panel />);
-
-if (module.hot) module.hot.accept();
