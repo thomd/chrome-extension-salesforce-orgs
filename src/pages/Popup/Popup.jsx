062ab4765c4b7d7ffb355a093f3739ea8263e0f3
Thomas Dürr <thomduerr@gmail.com>
2023-01-03 22:43:24 +0100

    icon

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import './Popup.css';
-import { ChakraProvider } from '@chakra-ui/react'
+import * as React from 'react'
+import { ChakraProvider, Container, Flex, VStack, IconButton, SearchIcon } from '@chakra-ui/react'
+
+// https://react-icons.github.io/react-icons
+import { TiCog } from 'react-icons/ti';
 
 const Popup = () => {
   return (
     <ChakraProvider>
-    <div className="App">
-      <header className="App-header">
-        <p>test</p>
-      </header>
-    </div>
+    <Container maxW="container.sm" p={0}>
+    <Flex h="100vh" p={20}>
+    <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
+    <p>test</p>
+    </VStack>
+    <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg="gray.50">
+    <IconButton aria-label='Search database' icon={<TiCog />} />
+    </VStack>
+    </Flex>
+    </Container>
     </ChakraProvider>
   );
 };
