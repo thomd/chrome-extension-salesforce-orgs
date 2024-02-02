f94418c12dd32854d07f0d7d85fe63a1739a102a
Thomas Dürr <thomduerr@gmail.com>
2024-02-02 23:59:28 +0100

    options menu

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -8,14 +8,15 @@ function Options({ exportConfig, importConfig }) {
     <Fragment>
       <Menu>
         <MenuButton as={IconButton} aria-label='Options' icon={<IconAction icon={BiMenu} />} variant='ghost' _hover={{ bg: 'none' }} _active={{ bg: 'none' }} />
-        <MenuList boxShadow='rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px'>
-          {/* <MenuItem>Add Org</MenuItem>
-          <MenuItem>Edit Orgs</MenuItem>
-          <MenuItem>Add Link</MenuItem>
-          <MenuItem>Edit Links</MenuItem>
-          <MenuDivider /> */}
-          <MenuItem fontSize='sm' fontWeight='bold' color='gray.400' _hover={{ bg: 'none', color: 'sf.headtc' }} _focus={{ bg: 'none' }} onClick={exportConfig}>Export Configuration</MenuItem>
-          <MenuItem fontSize='sm' fontWeight='bold' color='gray.400' _hover={{ bg: 'none', color: 'sf.headtc' }} onClick={importConfig}>Import Configuration</MenuItem>
+        <MenuList borderWidth={2} borderColor='gray.300'>
+          <MenuItem fontSize='sm' fontWeight='bold' color='gray.400' _hover={{ color: 'sf.headtc' }}>Add Org</MenuItem>
+          <MenuItem fontSize='sm' fontWeight='bold' color='gray.400' _hover={{ color: 'sf.headtc' }}>Edit Orgs</MenuItem>
+          {/* <MenuDivider />
+          <MenuItem fontSize='sm' fontWeight='bold' color='gray.400' _hover={{ color: 'sf.headtc' }}>Add Link</MenuItem>
+          <MenuItem fontSize='sm' fontWeight='bold' color='gray.400' _hover={{ color: 'sf.headtc' }}>Edit Links</MenuItem> */}
+          <MenuDivider />
+          <MenuItem fontSize='sm' fontWeight='bold' color='gray.400' _hover={{ color: 'sf.headtc' }} onClick={exportConfig}>Export Configuration</MenuItem>
+          <MenuItem fontSize='sm' fontWeight='bold' color='gray.400' _hover={{ color: 'sf.headtc' }} onClick={importConfig}>Import Configuration</MenuItem>
         </MenuList>
       </Menu>
     </Fragment>
