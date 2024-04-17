4008463a423d7b6ecb85d65a30b98329ecd8630d
Thomas Dürr <thomduerr@gmail.com>
2024-04-17 16:48:03 +0200

    more links

--- src/utils/defaultLinks.js
+++ src/utils/defaultLinks.js
@@ -19,6 +19,21 @@ const defaultLinks = {
             'id': 'link-4',
             'name': 'Salesforce Status',
             'href': 'status.salesforce.com'
+        },
+        {
+            'id': 'link-5',
+            'name': 'Workbench',
+            'href': 'workbench.developerforce.com'
+        },
+        {
+            'id': 'link-6',
+            'name': 'Salesforce Metadata Types',
+            'href': 'developer.salesforce.com/docs/atlas.en-us.api_meta.meta/api_meta/meta_types_list.htm'
+        },
+        {
+            'id': 'link-7',
+            'name': 'Salesforce Standard Objects',
+            'href': 'developer.salesforce.com/docs/atlas.en-us.object_reference.meta/object_reference/sforce_api_objects_list.htm'
         }
     ]
 }
